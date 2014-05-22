84a8d7abcb4f2a5f9e8cf01cf58531b3b93c985c
tbinkleyjones-edu <tbinkleyjones@tbinkleyjones-mbp.local>
2014-05-22 10:36:29 -0400

    Project creation

---
+++ ContactMgr/ContactMgrTests/ContactMgrTests.m
@@ -0,0 +1,34 @@
+//
+//  ContactMgrTests.m
+//  ContactMgrTests
+//
+//  Created by Timothy Binkley-Jones on 5/22/14.
+//  Copyright (c) 2014 msse650. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ContactMgrTests : XCTestCase
+
+@end
+
+@implementation ContactMgrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
