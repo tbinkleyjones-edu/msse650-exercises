17e33d354d288c844f5bd25bc0985c56bbaf4272
tbinkleyjones-edu <tbinkleyjones@tbinkleyjones-mbp.local>
2014-05-22 10:53:53 -0400

    Buttons added

--- ContactMgr/ContactMgr/AppDelegate.m
+++ ContactMgr/ContactMgr/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    ViewController *viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = viewController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ ContactMgr/ContactMgr/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  ContactMgr
+//
+//  Created by Timothy Binkley-Jones on 5/21/14.
+//  Copyright (c) 2014 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UITextField *phone;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+
+- (IBAction)saveContact:(id)sender;
+- (IBAction)deleteContact:(id)sender;
+
+@end
